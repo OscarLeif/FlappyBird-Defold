99041569293999a3826a993046b41082c04251f3
Oscar Fernandez <oscar.leif@gmail.com>
2024-07-21 20:39:42 -0500

    Ok we almost setup the game logic.
    The spawn should work on portrait and landscape mode.
    I'm thinking on how I should setup the Fade.
    Looks like the main collection should have a GUI layout to fade in out.
    This should be always active.
    But the game itself is almost ready.
    Great the game fails on real device....
    what could be then ?
    We need Visual studio code.
    it starts to be complex.

---
+++ utils/hashes.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.INPUT_FOCUS_GET=hash("acquire_input_focus")
+M.INPUT_FOCUS_RELEASE=hash("release_input_focus")
+M.INPUT_TOUCH=hash("touch")
+
+return M
---
+++ utils/lume.lua
@@ -0,0 +1,16 @@
+--
+-- lume
+--
+-- Copyright (c) 2016 rxi
+--
+-- This library is free software; you can redistribute it and/or modify it
+-- under the terms of the MIT license. See LICENSE for details.
+--
+local lume = {}
+
+function lume.clamp(x, min, max)
+	return x < min and min or (x > max and max or x)
+end
+
+
+return lume
