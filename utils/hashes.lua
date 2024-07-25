06f87029d17b76e3011c481401942b3036be5d42
Oscar Fernandez <oscar.leif@gmail.com>
2024-07-24 20:58:41 -0500

    Great we have playable game.
    The last step will be to save the best score data.

--- utils/hashes.lua
+++ utils/hashes.lua
@@ -9,7 +9,10 @@ M.PHYSICS_MESSAGE_CONTACT   = hash("contact_point_response")
 M.PHYSICS_MESSAGE_COLLISION = hash("collision_response")
 M.PHYSICS_MESSAGE_TRIGGER = hash("trigger_response")
 
+M.MESSAGE_SET_SCORE=hash("set_score")
+M.MESSAGE_INCREASE_SCORE=hash("increase_score")
 M.MESSAGE_GAME_OVER=hash("game_over")
 
+M.SCORE = hash("score")
 M.DIRT = hash("dirt")
 return M
