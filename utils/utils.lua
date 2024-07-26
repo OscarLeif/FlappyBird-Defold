5216dcc49859c07d1a39fa92cd982296fd191335
Oscar Fernandez <oscar.leif@gmail.com>
2024-07-26 00:41:00 -0500

    refactor some functions.
    I think this will make more easier to read the main_menu and game script files.
    we should completed the navigation and improve a little the UI.

---
+++ utils/utils.lua
@@ -0,0 +1,59 @@
+-- utils.lua
+
+local utils = {}
+
+local screen_width, screen_height
+
+function utils.init()
+    -- Initialize cached values
+    local w, h = window.get_size()
+    screen_width = w
+    screen_height = h
+end
+
+function utils.screen_to_world(sx, sy, sz, window_width, window_height, projection, view)
+    local inv = vmath.inv(projection * view)
+    sx = (2 * sx / window_width) - 1
+    sy = (2 * sy / window_height) - 1
+    sz = (2 * sz) - 1
+    local wx = sx * inv.m00 + sy * inv.m01 + sz * inv.m02 + inv.m03
+    local wy = sx * inv.m10 + sy * inv.m11 + sz * inv.m12 + inv.m13
+    local wz = sx * inv.m20 + sy * inv.m21 + sz * inv.m22 + inv.m23
+    return wx, wy, wz
+end
+
+--not sure if it's expensive to get the window width and height every time this is called
+function utils.screen_to_world_with_camera(sx, sy, sz, window_width, window_height, camera)
+    local projection = go.get(camera, "projection")
+    local view = go.get(camera, "view")
+    local inv = vmath.inv(projection * view)
+    sx = (2 * sx / window_width) - 1
+    sy = (2 * sy / window_height) - 1
+    sz = (2 * sz) - 1
+    local wx = sx * inv.m00 + sy * inv.m01 + sz * inv.m02 + inv.m03
+    local wy = sx * inv.m10 + sy * inv.m11 + sz * inv.m12 + inv.m13
+    local wz = sx * inv.m20 + sy * inv.m21 + sz * inv.m22 + inv.m23
+    return wx, wy, wz
+end
+
+function utils.get_camera_corner_points(camera)
+    local w, h = window.get_size()
+    local projection = go.get(camera, "projection")
+    local view = go.get(camera, "view")
+
+    local topLeftx, topLefty = utils.screen_to_world(0, h, 0, w, h, projection, view)
+    local topRightx, topRighty = utils.screen_to_world(w, h, 0, w, h, projection, view)
+    local bottomLeftx, bottomLefty = utils.screen_to_world(0, 0, 0, w, h, projection, view)
+    local bottomRightx, bottomRighty = utils.screen_to_world(w, 0, 0, w, h, projection, view)
+
+    local world_points = {
+        top_left = vmath.vector3(topLeftx, topLefty, 5),
+        top_right = vmath.vector3(topRightx, topRighty, 5),
+        bottom_left = vmath.vector3(bottomLeftx, bottomLefty, 5),
+        bottom_right = vmath.vector3(bottomRightx, bottomRighty, 5)
+    }
+
+    return world_points
+end
+
+return utils
