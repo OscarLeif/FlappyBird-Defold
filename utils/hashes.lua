18d97dfb8acc6185ddb4f5185d34cc929c6523e1
Oscar Fernandez <oscar.leif@gmail.com>
2024-07-30 11:00:18 -0500

    refactor the common hashes message constants
    Makes no sense do this now.
    I had to make since the start of the development.
    I didn't know how constants work at first.
    That's why now I'm fixing this.

--- utils/hashes.lua
+++ utils/hashes.lua
@@ -1,20 +1,31 @@
-local M = {}
-M.Main = msg.url("main:/main#main")
+local M                     = {}
+M.Main                      = msg.url("main_collection:/main#main")
 
-M.INPUT_FOCUS_GET=hash("acquire_input_focus")
-M.INPUT_FOCUS_RELEASE=hash("release_input_focus")
-M.INPUT_TOUCH=hash("touch")
+--Defold commons
+M.INPUT_FOCUS_GET           = hash("acquire_input_focus")
+M.INPUT_FOCUS_RELEASE       = hash("release_input_focus")
+M.INPUT_TOUCH               = hash("touch")
+M.LOAD                      = hash("load")           -- load a proxy
+M.UNLOAD                    = hash("unload")         -- unload proxy
+M.PROXY_LOADED              = hash("proxy_loaded")   --trigger event when a proxy load
+M.PROXY_UNLOADED            = hash("proxy_unloaded") --trigger event when a proxy unload
+M.ENABLE                    = hash("enable")
+M.DISABLE                   = hash("disable")
 
+-- Physics 2d (No sure about 3D)
 M.PHYSICS_MESSAGE_CONTACT   = hash("contact_point_response")
 M.PHYSICS_MESSAGE_COLLISION = hash("collision_response")
-M.PHYSICS_MESSAGE_TRIGGER = hash("trigger_response")
+M.PHYSICS_MESSAGE_TRIGGER   = hash("trigger_response")
 
-M.MESSAGE_SET_SCORE=hash("set_score")
-M.MESSAGE_INCREASE_SCORE=hash("increase_score")
-M.MESSAGE_GAME_OVER=hash("game_over")
+--sound
+M.MESSAGE_PLAY_SOUND        = hash("play_sound")
 
-M.MESSAGE_PLAY_SOUND=hash("play_sound")
+--Flappy bird game created for this game not common defold
+M.MESSAGE_SET_SCORE         = hash("set_score")
+M.MESSAGE_INCREASE_SCORE    = hash("increase_score")
+M.MESSAGE_GAME_OVER         = hash("game_over")
 
-M.SCORE = hash("score")
-M.DIRT = hash("dirt")
+M.SCORE                     = hash("score")
+M.DIRT                      = hash("dirt")
+
 return M
